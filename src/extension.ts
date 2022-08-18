ff6e9f947ef4d327661183d514429f0de59c77c7
leep-frog <gleeper49@gmail.com>
2022-08-17 23:29:41 -0400

    command to play audio

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,90 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-interface GroogTaskDefinition extends vscode.TaskDefinition {
+interface AudioTaskDefinition extends vscode.TaskDefinition {
   /**
    * The audio file to play.
    */
   file: string;
 }
 
+interface AudioCommandArgs {
+  /**
+   * The audio file to play.
+   */
+  file: string;
+}
+
+function createTask(name: string, source: string, definition: vscode.TaskDefinition, scope: vscode.TaskScope | vscode.WorkspaceFolder, file: string): vscode.Task {
+  let pyFile: string = [
+    "from playsound import playsound",
+    "import os",
+    "import sys",
+    "",
+    "p = os.path.abspath(sys.argv[1])",
+    "if not os.path.isfile(p):",
+    "  # TODO: use problem matcher here?",
+    "  print('not a file')",
+    "  exit(0)",
+    "",
+    // See the following answer for why this logic is needed:
+    // https://stackoverflow.com/a/68937955/18162937
+    "if os.name == 'nt':",
+    "  p = p.replace('\\\\', '\\\\\\\\', 1)",
+    "",
+    "playsound(p)",
+  ].join("\n");
+
+  let nt = new vscode.Task(
+    // This must always be the *exact same* definition
+    // from the provided task object.
+    definition,
+    scope,
+    name,
+    source,
+    new vscode.ShellExecution(
+      "audio-player",
+      {
+        executable: "python",
+        shellArgs: [
+          "-c",
+          `"${pyFile}"`,
+          file,
+        ]
+      },
+    ),
+  );
+
+  nt.presentationOptions.reveal = vscode.TaskRevealKind.Never;
+  nt.presentationOptions.showReuseMessage = false;
+  nt.problemMatchers = [];
+
+  return nt;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(vscode.commands.registerCommand('what-the-beep.playWithArgs', (args: AudioCommandArgs) => {
+    vscode.tasks.executeTask(createTask(
+      "audioPlayer",
+      "What the Beep?",
+      {
+        type: "audioPlayer",
+      },
+      vscode.TaskScope.Global,
+      args.file,
+      //"C:\\Users\\gleep\\Desktop\\Coding\\go\\src\\notification\\media\\laser.wav",
+    ));
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('what-the-beep.play', () => {
+    vscode.window.showInformationMessage("nothing");
+  }));
+
   context.subscriptions.push(vscode.tasks.registerTaskProvider('audioPlayer', {
     provideTasks: () => {
+      vscode.window.showInformationMessage("Pt");
       return [
         new vscode.Task(
           { type: "audioPlayer" },
@@ -25,52 +97,14 @@ export function activate(context: vscode.ExtensionContext) {
       ];
     },
     resolveTask(_task: vscode.Task): vscode.Task | undefined {
-      const definition: GroogTaskDefinition = <any>_task.definition;
-
-      let pyFile: string = [
-        "from playsound import playsound",
-        "import os",
-        "import sys",
-        "",
-        "p = os.path.abspath(sys.argv[1])",
-        "if not os.path.isfile(p):",
-        "  # TODO: use problem matcher here?",
-        "  print('not a file')",
-        "  exit(0)",
-        "",
-        // See the following answer for why this logic is needed:
-        // https://stackoverflow.com/a/68937955/18162937
-        "if os.name == 'nt':",
-        "  p = p.replace('\\\\', '\\\\\\\\', 1)",
-        "",
-        "playsound(p)",
-      ].join("\n");
-
-      let nt = new vscode.Task(
-        // This must always be the *exact same* definition
-        // from the provided task object.
-        definition,
-        _task.scope ?? vscode.TaskScope.Global,
+      const definition: AudioTaskDefinition = <any>_task.definition;
+      return createTask(
         _task.name,
         _task.source,
-        new vscode.ShellExecution(
-          "audio-player",
-          {
-            executable: "python",
-            shellArgs: [
-              "-c",
-              `"${pyFile}"`,
-              definition.file,
-            ]
-          },
-        ),
+        definition,
+        _task.scope ?? vscode.TaskScope.Global,
+        definition.file,
       );
-
-      nt.presentationOptions.reveal = vscode.TaskRevealKind.Never;
-      nt.presentationOptions.showReuseMessage = false;
-      nt.problemMatchers = [];
-
-      return nt;
     }
   }));
 }
