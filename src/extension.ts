8c1adae66b05f8603a366c24c7357b93d8acf10f
Greg Leeper <gleeper49@gmail.com>
2025-01-07 22:00:44 -0500

    use any audio file

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('what-the-beep.beep', () => webviewProvider.beep()),
+    vscode.commands.registerCommand('what-the-beep.beep', (args: BeepArgs) => webviewProvider.beep(args)),
     vscode.window.registerWebviewViewProvider(WebviewProvider.viewType, webviewProvider, {
       webviewOptions: {
         // This ensures that the widget still runs even when not visible.
@@ -24,6 +24,10 @@ export function activate(context: vscode.ExtensionContext) {
       },
     }),
   );
+}
+
+export interface BeepArgs {
+  filepath?: string;
 }
 
 class WebviewProvider implements vscode.WebviewViewProvider {
@@ -36,14 +40,21 @@ class WebviewProvider implements vscode.WebviewViewProvider {
     private readonly _extensionUri: vscode.Uri,
   ) { }
 
-  async beep() {
+  async beep(args?: BeepArgs) {
     // TODO: treat this as a user interaction somehow?
     // https://github.com/microsoft/vscode/issues/237030
     if (!this._view) {
       vscode.window.showErrorMessage(`What The Beep panel hasn't finished loading. Open the Explorer tab and click the checkbox to activate.`);
       return;
     }
-    this._view.webview.postMessage({});
+
+    console.log(`ARGS: ${JSON.stringify(args)}`);
+
+    const fileUri = args?.filepath ? vscode.Uri.file(args.filepath) : vscode.Uri.joinPath(this._extensionUri, 'media', 'success.wav');
+    const webviewUri = this._view.webview.asWebviewUri(fileUri);
+    this._view.webview.postMessage({
+      uri: `${webviewUri}`,
+    });
   };
 
   resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken): Thenable<void> | void {
@@ -58,12 +69,10 @@ class WebviewProvider implements vscode.WebviewViewProvider {
       ],
     };
 
-    const successWav = vscode.Uri.joinPath(this._extensionUri, 'media', 'success.wav');
-    const successWavUri = this._view.webview.asWebviewUri(successWav);
-
     const htmlPath = vscode.Uri.joinPath(this._extensionUri, 'src', 'webview.html').fsPath;
-    const got = readFileSync(htmlPath, 'utf8').replace(/AUDIO_PLACEHOLDER/g, `${successWavUri}`);
-    this._view.webview.html = got;
+    const fileUri = vscode.Uri.joinPath(this._extensionUri, 'media', 'success.wav');
+    const webviewUri = this._view.webview.asWebviewUri(fileUri);
+    this._view.webview.html = readFileSync(htmlPath, 'utf8').replace(/AUDIO_PLACEHOLDER/g, `${webviewUri.fsPath}`);
 
     let showOnce = true;
     this._view.webview.onDidReceiveMessage(() => {
