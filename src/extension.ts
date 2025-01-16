dd22a435b4a1aba6dfa2004cfe209eaeccf9c630
Greg Leeper <gleeper49@gmail.com>
2025-01-15 20:25:48 -0500

    it works!!!

--- src/extension.ts
+++ src/extension.ts
@@ -1,89 +1,39 @@
-import { readFileSync } from 'fs';
 import * as vscode from 'vscode';
-
-
-export function activate(context: vscode.ExtensionContext) {
-  const webviewProvider = new WebviewProvider(context.extensionUri);
-
-  // TODO: Use hooks like these to fake a user event?
-  // Sometimes seems to work, but other times not
-  // const activateHelpers = [
-  //   vscode.window.onDidChangeTextEditorSelection(() => {
-  //     vscode.window.showInformationMessage(`YUP`);
-  //     vscode.commands.executeCommand('what-the-beep.beep');
-  //   }),
-  // ];
-
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand('what-the-beep.beep', (args: BeepArgs) => webviewProvider.beep(args)),
-    vscode.window.registerWebviewViewProvider(WebviewProvider.viewType, webviewProvider, {
-      webviewOptions: {
-        // This ensures that the widget still runs even when not visible.
-        retainContextWhenHidden: true,
-      },
-    }),
-  );
-}
+const sound = require("sound-play");
 
 export interface BeepArgs {
   filepath?: string;
 }
 
-class WebviewProvider implements vscode.WebviewViewProvider {
 
-  public static readonly viewType = 'what-the-beep';
+export function activate(context: vscode.ExtensionContext) {
 
-  private _view?: vscode.WebviewView;
+  context.subscriptions.push(
+    vscode.commands.registerCommand('what-the-beep.beep', async (args?: BeepArgs) => {
 
-  constructor(
-    private readonly _extensionUri: vscode.Uri,
-  ) { }
+      const filepath = args?.filepath ?? vscode.Uri.joinPath(context.extensionUri, 'media', 'success.wav').fsPath;
+      console.log(`[What-the-beep] Playing audio file ${filepath}`);
 
-  async beep(args?: BeepArgs) {
-    // TODO: treat this as a user interaction somehow?
-    // https://github.com/microsoft/vscode/issues/237030
-    if (!this._view) {
-      vscode.window.showErrorMessage(`What The Beep panel hasn't finished loading. Open the Explorer tab and click the checkbox to activate.`);
-      return;
-    }
-
-    console.log(`ARGS: ${JSON.stringify(args)}`);
-
-    const fileUri = args?.filepath ? vscode.Uri.file(args.filepath) : vscode.Uri.joinPath(this._extensionUri, 'media', 'success.wav');
-    const webviewUri = this._view.webview.asWebviewUri(fileUri);
-    this._view.webview.postMessage({
-      uri: `${webviewUri}`,
-    });
-  };
-
-  resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken): Thenable<void> | void {
-    this._view = webviewView;
-    this._view.webview.options = {
-      // Allow scripts in the webview
-      enableScripts: true,
-
-      // Allows webview html to access relevant audio files.
-      localResourceRoots: [
-        this._extensionUri,
-      ],
-    };
-
-    const htmlPath = vscode.Uri.joinPath(this._extensionUri, 'src', 'webview.html').fsPath;
-    const fileUri = vscode.Uri.joinPath(this._extensionUri, 'media', 'success.wav');
-    const webviewUri = this._view.webview.asWebviewUri(fileUri);
-    this._view.webview.html = readFileSync(htmlPath, 'utf8').replace(/AUDIO_PLACEHOLDER/g, `${webviewUri.fsPath}`);
-
-    let showOnce = true;
-    this._view.webview.onDidReceiveMessage(() => {
-      vscode.window.showErrorMessage(`The webview that generates the audio may not have been created or enabled. Click the checkbox in the What The Beep pane in the Explorer tab to activate (or wait a few seconds for activation to complete). `);
-      if (showOnce) {
-        showOnce = false;
-        this._view?.show();
+      try {
+        await sound.play(filepath).then(
+          undefined,
+          (error: any) => {
+            vscode.window.showErrorMessage(`Failed to play audio file: ${error}`);
+          }
+        );
+      } catch (error) {
+        vscode.window.showErrorMessage(`Unexpected audio file error: ${error}`);
       }
-    });
-  }
+
+      console.log(`Successfully played audio file ${filepath}`);
+    }),
+  );
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+// TODO: Update sound-play to include the following in the windows script
+// (or just verify file exists and doesn't have any quotes or other special characters)
+// https://www.meziantou.net/stop-the-script-when-an-error-occurs-in-powershell.htm
+// $ErrorActionPreference = 'Stop' ; $PSNativeCommandUseErrorActionPreference = $true
